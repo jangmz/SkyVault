c347f70d8e82cc49674441e31eae15584f6ff629
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-11-14 14:09:03 +0100

    implemented sign up validation

--- routes/signUpRouter.js
+++ routes/signUpRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import signUpController from "../controllers/signUpController.js";
+import signUpValidation from "../validation/signUpValidation.js";
 
 const signUpRouter = Router();
 
 signUpRouter.get("/", signUpController.signUpGet);
-signUpRouter.post("/", signUpController.signUpPost);
+signUpRouter.post("/", signUpValidation, signUpController.signUpPost);
 
 export default signUpRouter;
