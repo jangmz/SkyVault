c76810866368dd2c5369d220da17a7c59cda7952
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-11-14 14:08:32 +0100

    queries for finding user via username and email

--- prisma/queries.js
+++ prisma/queries.js
@@ -2,12 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// creates a new user
 async function createUser(user) {
     try {
         const userData = await prisma.user.create({
             data: {
                 username: user.username,
-                password: user.password1,
+                password: user.password,
                 email: user.email
             }
         })
@@ -18,6 +19,32 @@ async function createUser(user) {
     }
 }
 
+// returns true/false if a user exists 
+async function usernameExists(username) {
+    const user = await prisma.user.findMany({
+        where: {
+            username: username,
+        }
+    })
+
+    if (user.length !== 0) return true;
+    else return false;
+}
+
+// returns true/false if email exists
+async function emailExists(email) {
+    const user = await prisma.user.findMany({
+        where: {
+            email: email
+        }
+    })
+
+    if (user.length !== 0) return true;
+    else return false;
+}
+
 export default {
     createUser,
+    usernameExists,
+    emailExists,
 }
