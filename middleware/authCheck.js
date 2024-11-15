a50bda7762f28c281c0a178d120c63152c2f3ff8
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-11-15 15:13:35 +0100

    updated

--- middleware/authCheck.js
+++ middleware/authCheck.js
@@ -1,11 +1,11 @@
 export function isAuthenticated(req, res, next) {
     if (req.isAuthenticated()) next();
     else {
-        const error = {
-            status: 401,
-            message: "Unauthorized",
-            details: "You are not authorized to view this page."
-        };
+        const error = new Error();
+
+        error.status = 401;
+        error.message = "Unauthorized";
+        error.details = "You are not authorized to view this page.";
 
         return next(error);
     }
@@ -13,7 +13,9 @@ export function isAuthenticated(req, res, next) {
 
 // sets the user to be accessed in ejs files
 export function setLocalsUser(req, res, next) {
-    if (req.isAuthenticated()) res.locals.user = req.user;
+    if (req.isAuthenticated()) {
+        res.locals.user = req.user;
+    }
 
     next();
 }
