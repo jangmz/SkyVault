adc9c5da368753a672d5b200400c602baeb5eaf2
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-11-15 14:53:22 +0100

    passport setup, serialization/deserialization

--- controllers/logInController.js
+++ controllers/logInController.js
@@ -1,14 +1,54 @@
+import db from "../prisma/queries.js";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import bcrypt from "bcryptjs";
+
+// passport
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            // find username in DB
+            const result = await db.findUserByUsername(username);
+            const user = result[0];
+            if (!user) return done(null, false, { message: "Incorrect username!" });
+
+            // password comparison
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) return done(null, false, { message: "Incorrect password!" });
+
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    })
+)
+
+// serialization/deserialization
+passport.serializeUser(async (user, done) => {
+    done(null, user.id); // save userID into the session
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await db.findUserById(id);
+        if (!user) {
+            console.error("User not found");
+            return done(null, false); // no user
+        }
+        done(null, user); // user object attached to REQ.USER
+    } catch (error) {
+        console.log("Error during deserialization.");
+        done(error);        
+    }
+})
+
+// GET /log-in -> form
 function logInGet(req, res, next) {
     res.render("log-in", {
         title: "Log In"
     });
 }
 
-function logInPost(req, res, next) {
-    // log in logic
-}
-
 export default {
     logInGet,
-    logInPost,
 }
