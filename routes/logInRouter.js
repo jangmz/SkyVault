81da99982dfc2c1b21240075c1f9c809035c6ae8
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-11-15 14:55:20 +0100

    passport authenticate anda redirections

--- routes/logInRouter.js
+++ routes/logInRouter.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import logInController from "../controllers/logInController.js";
+import passport from "passport";
 
 const logInRouter = Router();
 
 logInRouter.get("/", logInController.logInGet);
-logInRouter.post("/", logInController.logInPost);
+logInRouter.post("/", passport.authenticate("local", {
+    successRedirect: "/sky-vault",
+    failureRedirect: "/log-in",
+    failureFlash: true
+}));
 
 export default logInRouter;
