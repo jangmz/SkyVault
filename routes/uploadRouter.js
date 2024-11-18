a1f6d60cce6b80f7c6e8061683ed2606f7767b84
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-11-18 14:32:10 +0100

    updated

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
 import {isAuthenticated} from "../middleware/authCheck.js";
 import uploadController from "../controllers/uploadController.js";
-import multer from "multer";
-
-const upload = multer({ dest: "../public/uploads"});
-
 
 const uploadRouter = Router();
 
 uploadRouter.get("/", isAuthenticated, uploadController.uploadGet);
-uploadRouter.post("/", isAuthenticated, upload.single("file"), uploadController.uploadPost);
+uploadRouter.post("/", isAuthenticated, uploadController.upload.single("file"), uploadController.uploadPost);
 
 export default uploadRouter;
