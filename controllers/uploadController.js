880976ae6bd326fe094dfd47c43f84f46645a664
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-11-29 13:48:02 +0100

    comments removed

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -7,25 +7,6 @@ import supabase from "../supabase/supabase.js";
 const __filename = fileURLToPath(import.meta.url); // current file
 const __dirname = path.dirname(__filename); // current directory
 
-/*
-// configure multer storage
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../public/uploads")); // saves files to ./public/uploads
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, `${uniqueSuffix}-${file.originalname}`); // file name: 1687156034123-test.txt
-    }
-});
-
-// Multer upload instance
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit upload file size
-})
-*/
-
 // store files in memory temporarily
 const storage = multer.memoryStorage();
 const upload = multer({
