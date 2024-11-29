755e6b5b74388abc25caf7dc8452bdade0cd7cea
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-11-29 12:07:59 +0100

    implemented recursion for folder creation, also fixed folder list dropdown menu

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,11 +1,5 @@
 import db from "../prisma/queries.js";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "node:fs";
 import supabase from "../supabase/supabase.js";
-
-const __filename = fileURLToPath(import.meta.url); // current file
-const __dirname = path.dirname(__filename); // current directory
 
 // GET /folders/create -> form for creating a new folder
 async function folderGet(req, res, next) {
@@ -20,35 +14,40 @@ async function folderGet(req, res, next) {
 
 // POST /folders/create -> create new folder in the filesystem/cloud, add reference to the database
 async function folderPost(req, res, next) {
-    //const folderPath = path.join(__dirname, `../public/uploads/${req.body.foldername}`);
-    // TODO: Fix path if you are making folder inside a folder (recursion?)
-    const folderPath = `user-files/${req.user.username}/${req.body.foldername}`;
-
-    // creates folder reference for DB
-    const folderData = {
-        name: req.body.foldername,
-        created: new Date().toLocaleDateString(),
-        path: folderPath,
-        userID: req.user.id,
-        parentID: parseInt(req.body.parentFolder) || null,
-    }
-
-    /*
-    // create folder in the filesystem
-    try {
-        if (!fs.existsSync(folderData.path)) {
-            fs.mkdirSync(folderData.path);
+    // recursively building folder path
+    async function buildFolderPath(parentID, childName) {
+        if (!parentID) {
+            return `${req.user.username}/${childName}`; // root folder
         }
-    } catch (error) {
-        console.error(error);
+
+        const parentFolderData = await db.getFolderData(req.user.id, parseInt(parentID));
+        if(!parentFolderData) {
+            throw new Error("Parent folder not found");
+        }
+
+        const parentPath = await buildFolderPath(parentFolderData.parentID, parentFolderData.name); // call recursively if parent has a parent
+        return `${parentPath}/${childName}`;
     }
-    */ 
+
     try {
+        // get full path to the folder
+        const parentID = req.body.parentFolder ? parseInt(req.body.parentFolder) : null;
+        const folderPath = await buildFolderPath(parentID, req.body.foldername);
+
+        // creates folder reference for DB
+        const folderData = {
+            name: req.body.foldername,
+            created: new Date().toLocaleDateString(),
+            path: folderPath,
+            userID: req.user.id,
+            parentID: parentID,
+        }
+
         // create folder in supabase
-        await supabase.createFolder(req.user, folderData);
+        await supabase.createFolder(folderData);
         
         // insert folderData into DB
-        //await db.createFolder(folderData);
+        await db.createFolder(folderData);
     } catch (error) {
         return next(error);
     }
